3ef55ccf9f72e9059bdc74ff1b80ef0538b0e625
Dinu5346 <dinusha6326@outlook.com>
2022-10-28 15:46:16 +0530

    Sign up page partially coded

--- app/src/main/java/com/pizza4u/CustomerFragment.java
+++ app/src/main/java/com/pizza4u/CustomerFragment.java
@@ -1,12 +1,19 @@
 package com.pizza4u;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +21,15 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class CustomerFragment extends Fragment {
+
+    EditText editTextFirstNameCustomer;
+    EditText editTextLastNameCustomer;
+    EditText editTextEmailCustomer;
+    EditText editTextPhoneCustomer;
+    EditText editTextPasswordCustomer;
+    Button buttonAddProfilePictureCustomer;
+    Button buttonCreateAccountCustomer;
+    Button buttonCancelCustomer;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,5 +76,52 @@ public class CustomerFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_customer, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        editTextFirstNameCustomer = view.findViewById(R.id.editTextFirstNameCustomer);
+        editTextLastNameCustomer = view.findViewById(R.id.editTextLastNameCustomer);
+        editTextEmailCustomer = view.findViewById(R.id.editTextEmailCustomer);
+        editTextPhoneCustomer = view.findViewById(R.id.editTextPhoneCustomer);
+        editTextPasswordCustomer = view.findViewById(R.id.editTextPasswordCustomer);
+        buttonAddProfilePictureCustomer = view.findViewById(R.id.buttonAddProfilePictureCustomer);
+        buttonCreateAccountCustomer = view.findViewById(R.id.buttonCreateAccountCustomer);
+        buttonCancelCustomer = view.findViewById(R.id.buttonCancelCustomer);
+
+        buttonCancelCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case DialogInterface.BUTTON_POSITIVE:
+                                //Yes button clicked
+                                Intent intent = new Intent(getContext(),MainActivity.class);
+                                startActivity(intent);
+                                break;
+
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                //No button clicked
+                                break;
+                        }
+                    }
+                };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setMessage("Are you sure you want to cancel?").setPositiveButton("Yes", dialogClickListener)
+                        .setNegativeButton("No", dialogClickListener).show();
+            }
+        });
+
+        buttonAddProfilePictureCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/pizza4u/ManagerFragment.java
+++ app/src/main/java/com/pizza4u/ManagerFragment.java
@@ -1,12 +1,23 @@
 package com.pizza4u;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +25,17 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ManagerFragment extends Fragment {
+
+    EditText editTextFirstNameManager;
+    EditText editTextLastNameManager;
+    EditText editTextEmailManager;
+    EditText editTextPhoneManager;
+    EditText editTextPasswordManager;
+    EditText editTextBranchIdManager;
+    EditText editTextEmployeeIdManager;
+    Button buttonAddProfilePictureManager;
+    Button buttonCreateAccountManager;
+    Button buttonCancelManager;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,5 +82,99 @@ public class ManagerFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_manager, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        editTextFirstNameManager = view.findViewById(R.id.editTextFirstNameManager);
+        editTextLastNameManager = view.findViewById(R.id.editTextLastNameManager);
+        editTextEmailManager = view.findViewById(R.id.editTextEmailManager);
+        editTextPhoneManager = view.findViewById(R.id.editTextPhoneManager);
+        editTextPasswordManager = view.findViewById(R.id.editTextPasswordManager);
+        editTextBranchIdManager = view.findViewById(R.id.editTextBranchIdManager);
+        editTextEmployeeIdManager = view.findViewById(R.id.editTextEmployeeIdManager);
+        buttonAddProfilePictureManager = view.findViewById(R.id.buttonAddProfilePictureManager);
+        buttonCreateAccountManager = view.findViewById(R.id.buttonCreateAccountManager);
+        buttonCancelManager = view.findViewById(R.id.buttonCancelManager);
+
+        buttonCancelManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case DialogInterface.BUTTON_POSITIVE:
+                                //Yes button clicked
+                                Intent intent = new Intent(getContext(),MainActivity.class);
+                                startActivity(intent);
+                                break;
+
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                //No button clicked
+                                break;
+                        }
+                    }
+                };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setMessage("Are you sure you want to cancel?").setPositiveButton("Yes", dialogClickListener)
+                        .setNegativeButton("No", dialogClickListener).show();
+            }
+        });
+
+        buttonAddProfilePictureManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case DialogInterface.BUTTON_POSITIVE:
+                                //Yes button clicked
+                                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                startActivityForResult(pickPhoto , 1);
+                                break;
+
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                //No button clicked
+                                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                startActivityForResult(takePicture, 0);
+                                break;
+
+                            case DialogInterface.BUTTON_NEUTRAL:
+                                dialog.cancel();
+                                break;
+                        }
+                    }
+                };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setMessage("Select image from gallery or capture from camera?").setPositiveButton("Gallery", dialogClickListener)
+                        .setNegativeButton("Camera", dialogClickListener).setNeutralButton("Cancel",dialogClickListener).show();
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case 0:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+
+                }
+
+                break;
+            case 1:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+
+                }
+                break;
+        }
     }
 }
