478d864b750d9f8a7a3dca7747f4af7510203abf
Dinu5346 <dinusha6326@outlook.com>
2022-10-29 17:49:04 +0530

    Renamed CusReg as EmployeeReg

--- app/src/main/java/com/pizza4u/EmployeeRegActivity.java
+++ app/src/main/java/com/pizza4u/EmployeeRegActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CusRegActivity extends AppCompatActivity {
+public class EmployeeRegActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cus_reg);
+        setContentView(R.layout.activity_employee_reg);
     }
 }
