7390b6cc83fd034d97c8f41de56b3ba2a651b051
Dinu5346 <dinusha6326@outlook.com>
2022-10-29 18:06:26 +0530

    Fixed minor issues

--- app/src/main/java/com/pizza4u/ManagerMainActivity.java
+++ app/src/main/java/com/pizza4u/ManagerMainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class manager extends AppCompatActivity {
+public class ManagerMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
