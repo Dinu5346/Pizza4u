e5135e617cb9b1a0ef48efe010aac02d3eb3e095
Dinu5346 <dinusha6326@outlook.com>
2022-11-05 19:05:47 +0530

    Implemented Sign In functionality

--- app/src/main/java/com/pizza4u/SignInActivity.java
+++ app/src/main/java/com/pizza4u/SignInActivity.java
@@ -3,8 +3,10 @@ package com.pizza4u;
 import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +19,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -45,8 +49,46 @@ public class SignInActivity extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                                 if (task.isSuccessful()) {
+                                    if(!task.getResult().isEmpty()) {
                                     for (QueryDocumentSnapshot document : task.getResult()) {
-                                        Log.d(TAG, document.getId() + " => " + document.getData());
+                                            // Log.d(TAG, document.getId() + " => " + document.getData());
+                                            // String email = document.get("email").toString();
+                                            // Log.d("Email", email);
+
+                                            if(document.get("password").toString().equals(editTextPassword.getText().toString().trim())) {
+                                                Intent intent = new Intent(SignInActivity.this, CustomerMainActivity.class);
+                                                startActivity(intent);
+                                            } else {
+                                                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int which) {
+                                                        switch (which){
+                                                            case DialogInterface.BUTTON_POSITIVE:
+                                                                //Yes button clicked
+                                                                break;
+                                                        }
+                                                    }
+                                                };
+
+                                                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                                                builder.setMessage("Please enter correct password.").setPositiveButton("Ok", dialogClickListener)
+                                                        .show();
+                                            }
+                                    }} else {
+                                        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                switch (which){
+                                                    case DialogInterface.BUTTON_POSITIVE:
+                                                        //Yes button clicked
+                                                        break;
+                                                }
+                                            }
+                                        };
+
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                                        builder.setMessage("Please enter valid email.").setPositiveButton("Ok", dialogClickListener)
+                                                .show();
                                     }
                                 } else {
                                     Log.d(TAG, "Error getting documents: ", task.getException());
